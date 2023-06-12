bd39cb7484b22bf54c367fc01c9e02374c679b65
Ashok Kumar Kothinti <ashok.kothinti@gmail.com>
2023-06-12 20:26:22 +0530

    Update and rename floorplan-v1.0.js to floorplan-v1.1.js

--- floorplan-v1.1.js
+++ floorplan-v1.1.js
@@ -16,7 +16,7 @@ $('.cms-floorplans-studio-items > .cms-floorplans-studio-item').each(function(){
     }
 });
 
-$('.cms-floorplans-2bedroom-items > .cms-floorplans-2bedroom-item).each(function(){
+$('.cms-floorplans-2bedroom-items > .cms-floorplans-2bedroom-item').each(function(){
     $( ".2bedroom-large-slider" ).append( 
         "<li><div class='lb'></div><div class='floor-plans w-clearfix'><div class='floor-plans__left'><div class='bs-overlay-panel bs-overlay-background bs-overlay-top text-center text-uppercase'><h3 class='h2-heading-small'>" + $('.floor-plan-name', this).text() + "</h3><p class='mb0 mx-auto h4-heading t1'>" + $('.t1', this).text() + "</p><div class='mt-40 mb-40'><img src='https://uploads-ssl.webflow.com/6469c80d14f61c2d0623ed1e/6472eebbc6b654ee8608858b_fp-greendiv.png' alt='' /></div><p class='h4-heading mb0 mx-auto t2'>" + $('.t2', this).text() + "</p><p class='h4-heading mx-auto t3 mb-56'>" + $('.t3', this).text() + "</p><p class='pdfD mb-0'><a target='_blank' class='fp_pdf-download w-inline-block' href=" + $('.floor-plan-pdf', this).attr('href') + ">Download Floor Plan <br><img src='https://uploads-ssl.webflow.com/6469c80d14f61c2d0623ed1e/6472f15c63040bdcf06f66bc_dl-pdf.png' alt='' /></a></p></div></div><div class='floor-plans__right'><img src=" + $('.floor-plan-image', this).attr('src') +"></div></div></li>"		
     );
